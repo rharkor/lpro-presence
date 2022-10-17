d31f53740017f8fd93b1ed16b8171fb61bb14f80
louis <louis@huort.com>
2022-10-17 11:10:16 +0200

    update: modify the message

--- interactions.js
+++ interactions.js
@@ -5,26 +5,43 @@ const handleInteractions = (client) => {
     if (!interaction.isChatInputCommand()) return; // return if not a command
 
     if (interaction.commandName === "ping") {
-      await interaction.reply("Pong!");
+      try {
+        await interaction.reply({ content: "Pong!", ephemeral: true });
+      } catch (e) {
+        await interaction.reply({
+          content: "Une erreure s'est produite...",
+          ephemeral: true,
+        });
+      }
     }
 
     if (interaction.commandName === "subscribe") {
       try {
         await subscribe(interaction.user.id);
-        await interaction.reply("Vous avez correctement ajouté à la liste ✅");
+        await interaction.reply({
+          content: "Vous avez correctement été ajouté à la liste ✅",
+          ephemeral: true,
+        });
       } catch (e) {
-        await interaction.reply("Une erreure s'est produite...");
+        await interaction.reply({
+          content: "Une erreure s'est produite...",
+          ephemeral: true,
+        });
       }
     }
 
     if (interaction.commandName === "unsubscribe") {
       try {
         await unsubscribe(interaction.user.id);
-        await interaction.reply(
-          "Vous avez été retiré de la liste des messages privé"
-        );
+        await interaction.reply({
+          content: "Vous avez été retiré de la liste des messages privés",
+          ephemeral: true,
+        });
       } catch (e) {
-        await interaction.reply("Une erreure s'est produite...");
+        await interaction.reply({
+          content: "Une erreure s'est produite...",
+          ephemeral: true,
+        });
       }
     }
   });
